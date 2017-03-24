29dbc40e747a1e4f4b45e771377343c0c151c14c
Kareem <karimemad40@yahoo.com>
2017-03-24 15:19:13 +0200

    File Format added

--- Project_Main.m
+++ Project_Main.m
@@ -6,6 +6,7 @@ disp('\n');
 disp('program passed , press anykey to continue to Calculating InverseKinematics');
 pause;
 disp('\n');
+disp('The values of q1,q2,q3 are listed below');
 InverseKinematic(input(10),input(11),input(12),input(13),input(14),input(15));
 disp('\n');
 
@@ -13,6 +14,7 @@ disp('\n');
 disp('program passed , press anykey to continue to Calculating Max Torque');
 pause;
 disp('\n');
+disp('The values of max Torque for each are listed below');
 GetMaxTorque(input(16),input(17),input(18),input(19),input(20),input(21),input(22),input(23),input(24),[input(25) input(26) input(27)]')
 disp('\n');
 
